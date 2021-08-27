c5ac2b160ab8d109f2104b668ffa2ab390d398a4
Alexander Krasovskiy <alexandr.krasovskiy@gmail.com>
2021-08-27 18:10:44 +0300

    Main: added starting point

--- src/main/java/org/example/java_hw_3/Main.java
+++ src/main/java/org/example/java_hw_3/Main.java
@@ -1,4 +1,13 @@
 package org.example.java_hw_3;
 
 public class Main {
+    public static void main(String[] args) {
+        Model model = new Model();
+        View view = new View(model);
+
+        Controller controller = new Controller(
+                model,view);
+
+        controller.processUser();
+    }
 }
