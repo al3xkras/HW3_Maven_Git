b64924b522eeaaff16e970feb03bc3528cceef09
Alexander Krasovskiy <alexandr.krasovskiy@gmail.com>
2021-08-27 16:51:55 +0300

    Model (rand,setMinMaxValues) green test

--- src/main/java/org/example/java_hw_3/Model.java
+++ src/main/java/org/example/java_hw_3/Model.java
@@ -1,4 +1,17 @@
 package org.example.java_hw_3;
 
 public class Model {
+
+    public int minRandValue;
+    public int maxRandValue;
+
+    public void setMinMaxValues(int min, int max){
+        minRandValue = min;
+        maxRandValue = max;
+    }
+
+    public int rand(){
+        return minRandValue+(int)(Math.random()*(maxRandValue-minRandValue+1));
+    }
+
 }
